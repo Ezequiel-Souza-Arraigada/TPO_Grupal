c3349398c7c42db75069bd34efb53e23561c6316
Ezequiel-Souza-Arraigada <esouza@uade.edu.ar>
2024-09-24 20:06:13 -0300

    FEAT - TPO_Grupo3.py con matriz

--- TPO_Grupo3.py
+++ TPO_Grupo3.py
@@ -1,5 +1,78 @@
 diccionario_de_estudiantes = {}
 
+def dni_valido(dni):
+    return dni.isdigit() and 10000000 <= int(dni) <= 70000000
+
+def nota_valida(nota):
+    nota_int= int(nota)
+    if nota_int >= 0 and nota_int <= 10:
+        return nota
+    else:
+        return False
+
+def matriz(num_estudiantes):
+    matriz_datos = []
+
+    for f in range(num_estudiantes):
+        
+        nombre = input(f"Estudiante {f + 1} - Nombre: ").title()
+
+        # Validación del DNI
+        while True:
+            dni = input(f"Estudiante {f + 1} - DNI: ")
+            if dni_valido(dni):
+                break
+            else:
+                print("Error: DNI inválido. Debe estar entre 10,000,000 y 70,000,000.")
+        
+        # Validación de la Nota1
+        while True:
+            nota1 = input(f"Estudiante {f + 1} - Nota 1 (entre 0 y 10): ")
+            if nota_valida(nota1):
+                nota1 = int(nota1)
+                break
+            else:
+                print("Error: Nota inválida. Debe estar entre 0 y 10.")
+        
+        # Validación de la Nota2
+        while True:
+            nota2 = input(f"Estudiante {f + 1} - Nota 2 (entre 0 y 10): ")
+            if nota_valida(nota2):
+                nota2 = int(nota2)
+                break
+            else:
+                print("Error: Nota inválida. Debe estar entre 0 y 10.")
+        
+        # Cálculo nota final 
+        nota_final = (nota1 + nota2) // 2
+
+        # Agregar los datos validados a la matriz
+        matriz_datos.append([nombre, dni, nota1, nota2, nota_final])
+
+    return matriz_datos
+
+
+def imprimir_matriz_estudiantes(matriz_datos):
+
+    print("-- Lista de estudiantes --")
+    print("---------------------------------------------------------------------------------------")
+    
+    
+
+    # Bucle para imprimir cada fila de la matriz
+    for fila in matriz_datos:
+        nombre = fila[0]  
+        dni = fila[1]     
+        nota1 = fila[2]   
+        nota2 = fila[3]   
+        nota_final = fila[4] 
+        
+        
+        print(f"Nombre: {'%-15s' % nombre} | DNI: {'%-10s' % dni} | Nota 1: {nota1} | Nota 2: {nota2} | Nota Final: {nota_final}")
+    
+   
+    print("---------------------------------------------------------------------------------------")
+
 def agregar_nuevoestudiante():
     DNI = input("Ingrese su número de DNI (8 dígitos): ")
 
@@ -156,7 +229,8 @@ while True:
     print("5. Mostrar materias")
     print("6. Asignar materia a estudiante")
     print("7. Mostrar materias de un estudiante")
-    print("8. Salir")
+    print("8. Matriz calificaciones")
+    print("9. Salir")
 
     opcion = input("Seleccione una opción: ")
 
@@ -175,6 +249,10 @@ while True:
     elif opcion == '7':
         print(mostrar_materias_estudiante())
     elif opcion == '8':
+        num_estudiantes = int(input("¿Cuántos estudiantes quieres ingresar?: "))
+        matriz_prueba = matriz(num_estudiantes)
+        imprimir_matriz_estudiantes(matriz_prueba)
+    elif opcion == '9':
         break
     else:
         print("Opción no válida")
